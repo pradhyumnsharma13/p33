4b92952a37e54c828f70f3d6ba74b2a59621c5d8
pradhyumnsharma13 <81865891+pradhyumnsharma13@users.noreply.github.com>
2021-10-05 20:06:00 +0530

    Add files via upload

---
+++ Snowfall.js
@@ -0,0 +1,15 @@
+class Snowfall{
+    constructor()
+    {
+        var option={isStatic:false}
+        this.snowfall=Bodies.rectangle(random(1,800),10,10,10,option);  
+        World.add(world,this.snowfall); 
+    }
+    display()
+    {
+        //imageMode(RIGHT);
+        //imageMode(CENTRE);
+        imageMode(CENTER);
+        image(snowimage,this.snowfall.position.x,this.snowfall.position.y,10,10);
+    }
+}
